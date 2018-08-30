257cac2ad60f87addbaf86b211448ab8f38cd11f
Ricardo Lugo Recillas <ricardo.lugo.recillas@gmail.com>
2018-08-30 13:56:22 -0500

    ejemplo de http client

--- 04-spotiapp/src/app/components/home/home.component.ts
+++ 04-spotiapp/src/app/components/home/home.component.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  paises: any[] = [];
+  constructor( private http: HttpClient) {
+    this.http.get('https://restcountries.eu/rest/v2/capital/es').subscribe(
+      resp => { this.paises = resp });
+  }
 
   ngOnInit() {
   }
